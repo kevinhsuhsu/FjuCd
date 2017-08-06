bc6a30d16b052ad7bb6dae49a610f2519fe41660
KevinHsuHsu <ssh09830233@gmail.com>
2017-08-06 16:30:11 +0800

    首頁完成 最新消息

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
   typed: string;
   ngOnInit() {
     this.typed = new Typed('.typed', {
-      strings: ['歡迎來到輔大崇德志工社', '最新消息都在這裡', '可以網路報名參加活動'],
+      strings: ['歡迎來到崇德志工社', '最新消息都在這裡', '可以網路報名參加活動'],
       typeSpeed: 100,
       backDelay: 900,
       loop: true
--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -6,4 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.css']
 })
 
-export class NewsComponent { }
+export class NewsComponent { 
+  news_1 = '../../assets/images/bw-1.png';
+  news_2 = '../../assets/images/bw-2.png';
+  news_3 = '../../assets/images/bw-3.png';
+  news_4 = '../../assets/images/bw-4.png';
+  news_5 = '../../assets/images/bw-5.png';
+  news_6 = '../../assets/images/bw-6.png';
+}
